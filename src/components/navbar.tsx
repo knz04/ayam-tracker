a6003f8b6b1bd564172cd9a34bf2cf19f5933745
hawyeet <53439203+knz04@users.noreply.github.com>
2025-01-20 11:07:01 +0700

    Reapply "Update navbar.tsx"
    This reverts commit 0d3ea81990a78b5096c04a5e91c30b6199e365b4.

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -18,7 +18,7 @@ export default function Navbar() {
               className="btn btn-ghost btn-circle avatar"
             >
               <div className="w-10 rounded-full">
-                <Image
+                <img
                   alt="Tailwind CSS Navbar component"
                   src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
                 />
