49246bfc8cbc780615842ad02cf20388894a6bff
hawyeet <53439203+knz04@users.noreply.github.com>
2025-01-31 11:45:23 +0700

    fix: add loading to profile page

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { Suspense } from "react";
 import Image from "next/image";
 import { useState, useEffect } from "react";
 
@@ -8,6 +7,7 @@ export default function Profile() {
   const [username, setUsername] = useState<string>("");
   const [dateJoined, setDateJoined] = useState<string>("");
   const [userId, setUserId] = useState<string>("");
+  const [isLoading, setIsLoading] = useState<boolean>(true);
 
   useEffect(() => {
     const fetchProfile = async () => {
@@ -23,8 +23,7 @@ export default function Profile() {
         const fullDate = data.user.created_at.toString();
         setDateJoined(fullDate.split("T")[0]);
         setUserId(data.user.id);
-
-        console.log(data);
+        setIsLoading(false);
       } catch (error) {
         console.error(error);
       }
@@ -33,15 +32,20 @@ export default function Profile() {
     fetchProfile();
   }, []);
   return (
-    <Suspense
-      fallback={
-        <Image src="/loading.gif" alt="Loading..." width={50} height={50} />
-      }
-    >
-      <div className="flex flex-col items-center flex-grow">
-        <div className="card bg-base-100 w-full xl:w-1/2 shadow-xl animate-fade-up">
-          <div className="card-body">
-            <h2 className="card-title">Your Ayam Tracker Profile</h2>
+    <div className="flex flex-col items-center flex-grow">
+      <div className="card bg-base-100 w-full xl:w-1/2 shadow-xl animate-fade-up">
+        <div className="card-body">
+          <h2 className="card-title">Your Ayam Tracker Profile</h2>
+          {isLoading ? (
+            <div className="flex justify-center items-center py-5">
+              <Image
+                src="/loading.gif"
+                alt="Loading..."
+                width={50}
+                height={50}
+              />
+            </div>
+          ) : (
             <div className="flex">
               <div className="flex-none w-16 h-16 md:w-20 md:h-20 rounded-full bg-yellow-400 mt-4">
                 <span className="flex justify-center py-3 md:py-4 text-4xl md:text-5xl">
@@ -54,9 +58,9 @@ export default function Profile() {
                 <p className="text-xs text-base-300">User ID: {userId}</p>
               </div>
             </div>
-          </div>
+          )}
         </div>
       </div>
-    </Suspense>
+    </div>
   );
 }
